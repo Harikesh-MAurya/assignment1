974a92914fafe6813aa4fdf2f49d2f2add719792
Harikesh-MAurya <113936189+Harikesh-MAurya@users.noreply.github.com>
2022-09-21 21:52:38 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,43 @@
+# (question-1)..............
+# python --version or 
+
+# (question-2)...............
+from unicodedata import name
+
+
+print("Hello Python")
+
+# (question-4)..............
+print("Hello Python")
+
+# (question-5)..............
+print("Hello\nPython")
+
+# (question-6)..............
+print("MySirG")
+
+# (question-7)..............
+print("Teacher’s Day")
+
+# (question-8)..............
+a=5
+print("a =",a)
+
+# (question-9)...............
+name,age="harikesh",18
+qualification,year="B-Tech",2022
+print("name =",name,"\n","age =",age,"\n","qualification =",qualification,"\n","year =",year)
+
+# (question-10)...............
+# a=int(input("enter first number : "))
+# b=int(input("enter second number : "))
+# c=int(input("enter third number :  "))
+a,b,c=input("enter three number : ").split(",")
+a=int(a)
+b=int(b)
+c=int(c)
+print("a =",a,"\n","b =",b,"\n","c =",c)
+
+
+
+
